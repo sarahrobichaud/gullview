59c36853dc894c9f20ca520268eafc64b0297ae4
Sarah Robichaud <robichaudsarah@outlook.com>
2024-02-20 16:10:38 -0400

    Moved display animation handler

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
-import Gullview from "./Gullview";
-import { LightboxConfig } from "./types/Config";
+import Gullview from './Gullview';
+import { LightboxConfig } from './types/Config';
 
 export const build = (config: LightboxConfig) => {
-  return Gullview.build(config);
+	return Gullview.build(config);
 };
+
+build({
+	targetClass: 'lightbox__src',
+	animation: {
+		display: {
+			enabled: true,
+			duration: 600,
+			morph: {
+				duration: 600,
+			},
+		},
+	},
+	dock: {
+		enabled: true,
+		download: true,
+	},
+}).init();
--- src/types/Animation.ts
+++ src/types/Animation.ts
@@ -1,20 +1,25 @@
-import { DisplayAnimationHandler } from "../Animation";
+import { DisplayAnimationHandler } from '../animation/Display';
 
 export type AnimationHandler = DisplayAnimationHandler;
 
+export type AnimationDisplayMorphConfig = {
+	duration: number;
+};
+
 export type AnimationDisplayConfig = {
-  enabled: boolean;
-  duration: number;
-  next: string;
-  prev: string;
+	enabled: boolean;
+	duration: number;
+	next: string;
+	prev: string;
+	morph: AnimationDisplayMorphConfig;
 };
 
 export type AnimationConfig = {
-  display: AnimationDisplayConfig;
+	display: AnimationDisplayConfig;
 };
 
 export type CounterConfig = {
-  enabled: boolean;
-  y: "top" | "bottom";
-  x: "left" | "center" | "right";
+	enabled: boolean;
+	y: 'top' | 'bottom';
+	x: 'left' | 'center' | 'right';
 };
