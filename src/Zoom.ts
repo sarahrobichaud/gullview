c833f424e3dc15b3c3cc2d42ce6c94085510d173
Sarah Robichaud <robichaudsarah@outlook.com>
2024-02-21 15:27:45 -0400

    Fixed morph bug after zooming

--- src/Zoom.ts
+++ src/Zoom.ts
@@ -1,48 +1,71 @@
 import { UI } from './UI';
+import { CustomMouseEvent } from './lib/Event';
 import { LightboxConfig } from './types/Config';
+import Dock from './ui/Dock';
 import { offsetPos } from './utils/position';
 
 export type ZoomConfig = {
     enabled: boolean;
-    level: number;
     blockNative: boolean;
+    baseLevel: number;
 };
 
 const defaultZoom = {
     enabled: true,
-    level: 2,
+    baseLevel: 2,
     blockNative: true,
 } satisfies ZoomConfig;
 
 export default class ZoomHandler {
     public config = {} as ZoomConfig;
-    private ui: UI;
+    private _zoomLevel: number;
+    public ui: UI;
+
+    public isZoomed = false;
 
     constructor(ui: UI, config: LightboxConfig['zoom']) {
         this.config = { ...defaultZoom, ...config };
         this.ui = ui;
+        this.zoomLevel = 1;
 
         this.handleCursorOffset = this.handleCursorOffset.bind(this);
+    }
+
+    public get zoomLevel() {
+        return this._zoomLevel;
+    }
+
+    public set zoomLevel(level: number) {
+        this._zoomLevel = level;
     }
 
     private blockZoomHandler(e: WheelEvent) {
         if (e.ctrlKey) e.preventDefault(); //prevent zoom
     }
 
-    private zoom(
-        { offsetX, offsetY }: { offsetX: number; offsetY: number },
-        level = this.config.level
-    ) {
-        this.ui.display.element.style.transformOrigin = `${offsetX * 100}% ${
-            offsetY * 100
-        }%`;
-        this.ui.display.element.classList.add('zoomed');
-        this.ui.elements.prev.style.visibility = 'hidden';
-        this.ui.elements.next.style.visibility = 'hidden';
-        this.ui.display.element.style.transform = `scale(${level})`;
+    public zoom = (
+        { offsetX, offsetY }: { offsetX: number; offsetY: number } = {
+            offsetX: 0.5,
+            offsetY: 0.5,
+        },
+        level = this.zoomLevel
+    ) => {
+        if (!this.isZoomed) {
+            this.startTrackingMouse();
+            this.ui.display.element.style.transformOrigin = `${
+                offsetX * 100
+            }% ${offsetY * 100}%`;
+            this.ui.display.element.classList.add('zoomed');
+            this.ui.elements.prev.style.visibility = 'hidden';
+            this.ui.elements.next.style.visibility = 'hidden';
+            this.ui.display.element.style.transform = `scale(${level})`;
+            this.isZoomed = true;
+            return;
+        }
 
-        window.addEventListener('mousemove', this.handleCursorOffset);
-    }
+        this.ui.display.element.style.transform = `scale(${level})`;
+        this.zoomLevel = level;
+    };
 
     private handleCursorOffset(e: MouseEvent) {
         const { clientX, clientY } = e;
@@ -54,11 +77,22 @@ export default class ZoomHandler {
     }
 
     public unzoom = () => {
+        this.stopTrackingMouse();
         this.ui.display.element.classList.remove('zoomed');
         this.ui.display.element.style.transform = '';
+        this.ui.display.element.style.transformOrigin = '';
         this.ui.elements.prev.style.visibility = 'visible';
         this.ui.elements.next.style.visibility = 'visible';
+        this.zoomLevel = 1;
+        this.isZoomed = false;
+    };
+
+    public stopTrackingMouse = () => {
         window.removeEventListener('mousemove', this.handleCursorOffset);
+    };
+
+    public startTrackingMouse = () => {
+        window.addEventListener('mousemove', this.handleCursorOffset);
     };
 
     public allowNative = () => {
@@ -73,14 +107,20 @@ export default class ZoomHandler {
     };
 
     public listener = ({ clientX, clientY, target }: MouseEvent) => {
-        if (!(target instanceof HTMLDivElement)) return;
+        if (!(target instanceof HTMLImageElement)) return;
 
         const bounds = target.getBoundingClientRect();
         const offsets = offsetPos(clientX, clientY, bounds);
-        if (this.ui.display.element.style.transform === '') {
+        if (!this.isZoomed) {
+            this.zoomLevel = this.config.baseLevel;
             this.zoom(offsets);
         } else {
             this.unzoom();
         }
     };
+    // public createDockElement = (dock: Dock) => {
+    //     const element = new ZoomElement(dock, this);
+    //     this.element = element;
+    //     return this.element;
+    // };
 }
